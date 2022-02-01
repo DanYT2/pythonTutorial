26c5f94dcae7b931c2959e0f195beb1faa9bec50
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-01 16:17:37 +0300

    Created a mad libs game
    Re-iterates the use of strings

---
+++ 8. Mad Libs Game.py
@@ -0,0 +1,7 @@
+color = input('Choose a colour: ')
+noun = input('Choose a plural noun: ')
+celebrity = input('Select a celebrity: ')
+
+print('Roses are ' + color)
+print(noun + ' are blue')
+print('I love ' + celebrity)
