f41a8c43cbcea6ef4804754b0d8d5f0321dc773c
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-01 23:19:19 +0300

    If statements
    Declaration and comparison operators (and, not, or)

---
+++ 12. If statements.py
@@ -0,0 +1,10 @@
+# Simple declaration
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+    print('You are Male and you are tall')
+elif is_male and not is_tall:
+    print('You are male, but you are short')
+else:
+    print('You are female')
