d484523533304cea506480471dc5cad3bcab0e08
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-04 20:04:09 +0300

    Inheriting attributes from classes.
    Method overriding also introduced

---
+++ 28. Inheritance.py
@@ -0,0 +1,8 @@
+from ChefClass import Chef
+from ChineseChefClass import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_fried_rice()
---
+++ ChineseChefClass.py
@@ -0,0 +1,10 @@
+from ChefClass import Chef
+
+
+class ChineseChef(Chef):
+    def make_fried_rice(self):
+        return print('The chef makes fried rice')
+
+    def make_special_dish(self):
+        return print('Chef makes orange chicken')
+   
